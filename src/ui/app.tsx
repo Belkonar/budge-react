b8d5de35db8f62067c54b0d235527a1cda74fad0
joseph Dotson <josephdotson@trollcavedev.com>
2023-09-27 22:49:58 -0400

    MUI

--- src/ui/app.tsx
+++ src/ui/app.tsx
@@ -12,24 +12,64 @@ import {
 } from "react-router-dom";
 import Home from './components/home';
 import Nav from './nav';
+import { AppBar, Box, Button, CssBaseline, Drawer, IconButton, List, ListItem, ListItemButton, ListItemText, ThemeProvider, Toolbar, Typography, createTheme } from '@mui/material';
+import MenuIcon from '@mui/icons-material/Menu';
+import { dark } from '@mui/material/styles/createPalette';
 
 function Root() {
+  const [state, setState] = React.useState({
+    open: false,
+  });
+
+  const nav = useNavigate();
+
+  const toggleDrawer = (open: boolean) => (event: React.KeyboardEvent | React.MouseEvent) => {
+    setState({ ...state, open: open });
+  }
+
+  const send = (location: string) => {
+    toggleDrawer(false);
+    nav(location);
+  }
+
   return <div>
-    <div data-bs-theme="dark">
-      <nav data-bs-theme="dark" className="navbar bg-body-tertiary">
-        <div className="container-fluid">
-          <span className="navbar-brand mb-0 h1">Budge</span>
-        </div>
-      </nav>
-      <div className='container-fluid content-container'>
-        <div className="row">
-          <Nav />
-          <div className="col">
-            <Outlet />
-          </div>
-        </div>
-      </div>
-    </div>
+    <AppBar position='static'>
+      <Toolbar variant='dense'>
+        <IconButton
+          size="large"
+          edge="start"
+          color="inherit"
+          aria-label="menu"
+          sx={{ mr: 2 }}
+          onClick={toggleDrawer(true)}
+        >
+          <MenuIcon />
+        </IconButton>
+        <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
+          Budge
+        </Typography>
+
+      </Toolbar>
+    </AppBar>
+    <Drawer
+      anchor="left"
+      open={state.open}
+      onClose={toggleDrawer(false)}>
+      <Box
+        sx={{ width: 250 }}
+        role="presentation">
+        <List>
+          <ListItem disablePadding>
+            <ListItemButton onClick={() => send('/')}>
+              <ListItemText>
+                Hi
+              </ListItemText>
+            </ListItemButton>
+          </ListItem>
+        </List>
+      </Box>
+    </Drawer>
+    <Outlet />
   </div>
 }
 
@@ -52,9 +92,18 @@ function render() {
     }
   ]);
 
+  const darkTheme = createTheme({
+    palette: {
+      mode: 'dark',
+    },
+  });
+
   root.render(
     <React.StrictMode>
-      <RouterProvider router={router} />
+      <ThemeProvider theme={darkTheme}>
+        <CssBaseline />
+        <RouterProvider router={router} />
+      </ThemeProvider>
     </React.StrictMode>
   );
 }
