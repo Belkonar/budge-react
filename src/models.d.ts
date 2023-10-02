eaefd1460acf562c2042ec31fce3feb9dc8e79b0
Tom <josephdotson@trollcavedev.com>
2023-10-02 18:27:46 -0400

    Started models

--- src/models.d.ts
+++ src/models.d.ts
@@ -2,3 +2,19 @@ interface DataApi {
   query: () => Promise<string>;
   exec: () => Promise<string>;
 }
+
+interface Account {
+  _id: string;
+  name: string;
+  type: 'debit' | 'credit';
+}
+
+interface Transaction {
+  _id: string;
+  timestamp: number;
+  description: string;
+  accountId: string;
+  payeeId?: string; // TODO: later
+  categoryId?: string;
+  balanced: boolean;
+}
