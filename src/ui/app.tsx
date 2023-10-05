29f0ef60cc34d269472b58ea108a9e8065e2e36c
joseph Dotson <josephdotson@trollcavedev.com>
2023-10-05 13:25:47 -0400

    strict

--- src/ui/app.tsx
+++ src/ui/app.tsx
@@ -101,7 +101,7 @@ function AppRoot() {
 }
 
 function render() {
-  const root = ReactDOM.createRoot(document.getElementById('root'));
+  const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement);
 
   root.render(
     <React.StrictMode>
